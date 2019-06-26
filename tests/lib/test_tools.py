1037847bcd27b807cfd2ba2f832d03f123e0dfac
Michael Wood <michael.wood@opendataservices.coop>
2019-06-26 20:18:48 +0100

    tests test_tools Add test for ignore_errors
    Make sure ignore_errors behaves in the way we expect

--- tests/lib/test_tools.py
+++ tests/lib/test_tools.py
@@ -1,6 +1,6 @@
 import pytest
 import os
-from libcove.lib.tools import get_file_type
+from libcove.lib.tools import get_file_type, ignore_errors
 from libcove.lib.exceptions import UnrecognisedFileType
 
 
@@ -29,3 +29,71 @@ def test_get_file_type_json_noextension_string():
 def test_get_file_unrecognised_file_type_string():
     with pytest.raises(UnrecognisedFileType):
         get_file_type('test')
+
+
+def test_ignore_errors():
+    """ Test the ignore_errors function """
+
+    # Test data that would in real usage come from having parsed a json
+    # document
+    test_data = {
+        'A': [
+            {'B': 'C'},
+            {'F': 'G'},
+        ],
+        'E': object(),
+    }
+
+    class obj_with_attr(object):
+        test = None
+
+    @ignore_errors
+    def check_data(json_data):
+        # KeyError
+        json_data['B']
+        # IndexError
+        json_data['A'][2]
+        # TypeError
+        json_data['A'][0]['B'] + 2
+        # AttributeError
+        json_data['E'].test
+        # ValueError
+        int(json_data['A'][1]['F'])
+
+    # Should pass without any exceptions
+    check_data(test_data, ignore_errors=True)
+
+    # Work our way down "correcting" the errors so we can
+    # Check each exception is captured as expected
+    try:
+        check_data(test_data, ignore_errors=False)
+    except KeyError:
+        pass
+
+    try:
+        test_data['B'] = "exist"
+        check_data(test_data, ignore_errors=False)
+    except IndexError:
+        pass
+
+    try:
+        test_data['A'].append({})
+        check_data(test_data, ignore_errors=False)
+    except TypeError:
+        pass
+
+    try:
+        test_data['A'][0]['B'] = 1
+        check_data(test_data, ignore_errors=False)
+    except AttributeError:
+        pass
+
+    try:
+        test_data['E'] = obj_with_attr()
+        check_data(test_data, ignore_errors=False)
+    except ValueError as e:
+        print("Got %s " % e)
+        return
+
+    # Should not be reached!
+    assert False, "All errors weren't tested"
