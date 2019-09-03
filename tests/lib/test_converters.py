37ca561c4de438832982e2d53322c109160aa592
Jared Parnell <jared.parnell@opendataservices.coop>
2019-09-03 12:31:37 +0100

    tests: provide an XML based test and fixture

--- tests/lib/test_converters.py
+++ tests/lib/test_converters.py
@@ -44,6 +44,42 @@ def test_convert_json_1():
         assert row2[1] == 'Hat'
 
 
+def test_convert_xml_1():
+
+    cove_temp_folder = tempfile.mkdtemp(prefix='lib-cove-ocds-tests-', dir=tempfile.gettempdir())
+    xml_filename = os.path.join(os.path.dirname(
+        os.path.realpath(__file__)), 'fixtures', 'converters', 'convert_1.xml'
+    )
+
+    lib_cove_config = LibCoveConfig()
+    output = convert_json(cove_temp_folder, "", xml_filename, lib_cove_config, flatten=True,
+                          xml=True, root_list_path='iati-activity')
+
+    assert output['converted_url'] == '/flattened'
+    assert len(output['conversion_warning_messages']) == 0
+    assert output['conversion'] == 'flatten'
+
+    conversion_warning_messages_name = os.path.join(cove_temp_folder, "conversion_warning_messages.json")
+    assert os.path.isfile(conversion_warning_messages_name)
+    with open(conversion_warning_messages_name) as fp:
+        conversion_warning_messages_data = json.load(fp)
+    assert conversion_warning_messages_data == []
+
+    assert os.path.isfile(os.path.join(cove_temp_folder, "flattened.xlsx"))
+    assert os.path.isfile(os.path.join(cove_temp_folder, "flattened", "iati-activity.csv"))
+
+    with open(os.path.join(cove_temp_folder, "flattened", "iati-activity.csv"), 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+
+        header = next(csvreader)
+        assert header[0] == '@default-currency'
+        assert header[1] == 'iati-identifier'
+
+        row1 = next(csvreader)
+        assert row1[0] == 'GBP'
+        assert row1[1] == 'GB-TEST-13-example_ODSC_2019'
+
+
 def test_convert_json_root_is_list_1():
 
     cove_temp_folder = tempfile.mkdtemp(prefix='lib-cove-ocds-tests-', dir=tempfile.gettempdir())
