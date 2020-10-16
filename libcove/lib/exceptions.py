78030713d8bef25905087f113bc8431d9a0e3cd8
Ben Webb <ben.webb@opendataservices.coop>
2020-10-16 17:21:36 +0000

    lib: Move input error messages to lib-cove-web
    https://github.com/open-contracting/cove-oc4ids/issues/59

--- libcove/lib/exceptions.py
+++ libcove/lib/exceptions.py
@@ -4,26 +4,18 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+class UnrecognisedFileType(Exception):
+    pass
+
+
 class CoveInputDataError(Exception):
     """
     An error that we think is due to the data input by the user, rather than a
     bug in the application.
     """
 
-    def __init__(self, context=None):
-        if context:
-            self.context = context
-
-
-class UnrecognisedFileType(CoveInputDataError):
-    context = {
-        "sub_title": _("Sorry, we can't process that data"),
-        "link": "index",
-        "link_text": _("Try Again"),
-        "msg": _(
-            "We did not recognise the file type.\n\nWe can only process json, csv, ods and xlsx files."
-        ),
-    }
+    def __init__(self, wrapped_err):
+        self.wrapped_err = wrapped_err
 
 
 def cove_spreadsheet_conversion_error(func):
@@ -33,16 +25,6 @@ def cove_spreadsheet_conversion_error(func):
             return func(request, *args, **kwargs)
         except Exception as err:
             logger.exception(err, extra={"request": request})
-            raise CoveInputDataError(
-                {
-                    "sub_title": _("Sorry, we can't process that data"),
-                    "link": "index",
-                    "link_text": _("Try Again"),
-                    "msg": _(
-                        "We think you tried to supply a spreadsheet, but we failed to convert it."
-                        "\n\nError message: {}".format(repr(err))
-                    ),
-                }
-            )
+            raise CoveInputDataError(wrapped_err=err)
 
     return wrapper
