105f2d81d08f2bab673819d68f05c54378174254
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-04-15 17:17:08 -0400

    requirements: Remove unused dependencies

--- setup.py
+++ setup.py
@@ -12,9 +12,7 @@ setup(
     install_requires=[
         "jsonref",
         "jsonschema",
-        "CommonMark",
         "requests",
-        "json-merge-patch",
         "cached-property",
         "flattentool>=0.11.0",
         # Required for jsonschema to validate URIs
