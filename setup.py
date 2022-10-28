5220e0e3a4fc552143174b9e3c11b14744d9771a
James <james.baster@opendataservices.coop>
2022-10-28 12:20:13 +0100

    release: 0.27.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="libcove",
-    version="0.27.0",
+    version="0.27.1",
     author="Open Data Services",
     author_email="code@opendataservices.coop",
     url="https://github.com/OpenDataServices/lib-cove",
